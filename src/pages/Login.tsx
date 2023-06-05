7cfc5eeaa9464dd958698a05078e4d8f16b661d1
gsmartinez0 <gsmartinez@google.com>
2023-06-05 11:21:34 -0700

    Auth setup started

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,34 @@
 import React from "react";
 import "../styles/Login.css";
 import g_logo from "../assets/g_logo.png";
-import { database, storage, auth } from "../firebase-config";
+import { database, storage, auth, provider } from "../firebase-config";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
-function Login() {
+export default function Login() {
+  const auth = getAuth();
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      // let token: string = "";
+      // if (credential) {
+      //   token = credential.accessToken!;
+      // }
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
   return (
     <>
       <div className="Login">
@@ -19,5 +44,3 @@ function Login() {
     </>
   );
 }
-
-export default Login;
