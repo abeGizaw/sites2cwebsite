43a952772bd53f24d50106a590abb3415a7a2dab
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-06-05 10:12:59 -0700

    Basic display for form input

---
+++ src/Components/Card.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+interface CardProps {
+  title: string;
+  description: string;
+  imageUrl: string;
+  index: number;
+}
+export default function CardComponent({
+  title,
+  description,
+  imageUrl,
+  index,
+}: CardProps) {
+  return (
+    <Card sx={{ maxWidth: 345 }} key={index}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="250"
+          image={`https://picsum.photos/id/${index}/300`}
+          alt="https://picsum.photos"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
---
+++ src/Components/addCard.tsx
@@ -0,0 +1,27 @@
+import { Container } from "@mui/material";
+import React from "react";
+import "../styles/homeScreen.css";
+
+export default function CardForm({ visibility }: { visibility: string }) {
+  function handleDisplay() {
+    visibility = "none";
+  }
+
+  return (
+    <Container
+      maxWidth="sm"
+      className="addCardContainer"
+      style={{ display: visibility }}
+    >
+      {" "}
+      <form className="form-container">
+        <input type="text" id="title" placeholder="Title"></input>
+        <input type="author" id="author" placeholder="Author"></input>
+        <input type="pages" id="pages" placeholder="Pages"></input>
+        <button type="submit" onClick={handleDisplay}>
+          Submit
+        </button>
+      </form>
+    </Container>
+  );
+}
