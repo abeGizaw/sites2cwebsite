9195503e950fd4388f6c8212e0c62136607ed757
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-06-07 15:07:31 -0700

    cards now pop up on their own screens

--- src/pages/cardScreen/CardScreen.tsx
+++ src/pages/cardScreen/CardScreen.tsx
@@ -1,25 +1,53 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "../../styles/Login.css";
 import { CardProps } from "../../Components/Cards/Card";
 import CardComponent from "../../Components/Cards/Card";
 import ResponsiveAppBar from "../../Components/Navbar/Navbar";
-import { DataSnapshot } from "firebase/database";
+import { DataSnapshot, update } from "firebase/database";
 import getCurrentCard from "./cardScreenUtils";
 
 export default function CardScreen() {
+  const [currentCard, setCurrentCard] = useState<CardProps>();
+
   const location = useLocation();
   const currentUrl = location.pathname;
   const postKey = currentUrl.substring(currentUrl.lastIndexOf("/") + 1);
 
   console.log(postKey);
 
+  function updateCurrentCard(cardTpUpdate: CardProps) {
+    setCurrentCard(() => cardTpUpdate);
+  }
+
   useEffect(() => {
-    getCurrentCard(postKey);
-  });
+    getCurrentCard(postKey).then((snapshot: DataSnapshot) => {
+      const data = snapshot.val();
+      console.log(data);
+      console.log(data.title);
+
+      const currentCardInfo: CardProps = {
+        title: data.cardTitle,
+        description: data.cardDescription,
+        imageUrl: data.cardImage,
+      };
+
+      updateCurrentCard(currentCardInfo);
+    });
+  }, [postKey]);
+
   return (
     <div className="container-xxl" id="singleCardScreen">
       <ResponsiveAppBar />
+      {currentCard && (
+        <CardComponent
+          postKey={postKey}
+          title={currentCard.title} // Provide appropriate values for title, description, and imageUrl
+          description={currentCard.description}
+          imageUrl={currentCard.imageUrl}
+          key={0}
+        />
+      )}
     </div>
   );
 }
--- src/pages/cardScreen/cardScreenUtils.ts
+++ src/pages/cardScreen/cardScreenUtils.ts
@@ -2,7 +2,6 @@ import { ref, get, update } from "firebase/database";
 import { database, auth } from "../../firebase-config";
 import { User } from "firebase/auth";
 
-export default function getCurrentCard(currentPostKey: string) {
-  console.log(currentPostKey);
-  return 3;
+export default async function getCurrentCard(currentPostKey: string) {
+  return await get(ref(database, `posts/${currentPostKey}`));
 }
