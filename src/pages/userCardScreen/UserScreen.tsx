0c5e6e4848151ee1cf232481a3e1d8decf7391bd
abegizaw3 <135074156+abegizaw3@users.noreply.github.com>
2023-06-08 13:51:45 -0700

    user can see their own posts

--- src/pages/userCardScreen/UserScreen.tsx
+++ src/pages/userCardScreen/UserScreen.tsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import ResponsiveAppBar from "../../Components/Navbar/Navbar";
 import getMyPostKeys from "./userScreenUtils";
 import { DataSnapshot } from "firebase/database";
-import { CardProps } from "../../Components/Cards/Card";
+import CardComponent, { CardProps } from "../../Components/Cards/Card";
 
 export default function UserScreen() {
+  const [currentUserCards, setCurrentUserCards] = useState<CardProps[]>([]);
+  const [currentUserPostkeys, setCurrentUserPostkeys] = useState<string[]>([]);
+
   const navigate = useNavigate();
 
   const location = useLocation();
@@ -17,14 +20,41 @@ export default function UserScreen() {
     getMyPostKeys(userId).then((snapshot: DataSnapshot) => {
       // Handle the snapshot data here
       const data = snapshot.val();
-      const allPostKeys: string[] = Object.values(data) as string[];
-      console.log(allPostKeys);
+      let postKeys: string[] = Object.keys(data) as string[];
+
+      setCurrentUserPostkeys(postKeys);
+
+      const cardDataArray: CardProps[] = (
+        Object.values(data) as Array<{
+          cardTitle: string;
+          cardDescription: string;
+          cardImage: string;
+        }>
+      ).map((currentEntry, index) => ({
+        title: currentEntry.cardTitle,
+        description: currentEntry.cardDescription,
+        imageUrl: currentEntry.cardImage,
+      }));
+      console.log(cardDataArray);
+      setCurrentUserCards(cardDataArray);
     });
   }, []);
 
   return (
     <div className="container-xxl" id="userScreen">
       <ResponsiveAppBar />
+      <div className="CardContainer">
+        {/* //fix the key */}
+        {currentUserCards.map((currentPost, index) => (
+          <CardComponent
+            title={currentPost.title} // Provide appropriate values for title, description, and imageUrl
+            description={currentPost.description}
+            imageUrl={currentPost.imageUrl}
+            postKey={currentUserPostkeys[index]}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
